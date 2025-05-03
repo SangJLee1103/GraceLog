df041ec2505deebe8fbde86043e44ae145aae0c7
SangJLee1103 <sangjlee1103@gmail.com>
2025-05-03 16:55:01 +0900

    feat: 토큰 갱신 (AuthenticationCredential, Authenticator, AuthenticationInterceptor) 구현 중

---
+++ GraceLog/Source/Data/Network/OAuth/GraceLogAuthenticator.swift
@@ -0,0 +1,62 @@
+//
+//  GraceLogAuthenticator.swift
+//  GraceLog
+//
+//  Created by 이상준 on 4/28/25.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+class GraceLogAuthenticator: Authenticator {
+    typealias Credential = GraceLogAuthenticationCredential
+    private let baseURL: String
+    private let disposeBag = DisposeBag()
+    
+    init(baseURL: String = "https://api.gracelog.com") {
+        self.baseURL = baseURL
+    }
+    
+    
+    func apply(_ credential: GraceLogAuthenticationCredential, to urlRequest: inout URLRequest) {
+        urlRequest.headers.add(.authorization(bearerToken: credential.accessToken))
+        urlRequest.addValue(credential.refreshToken, forHTTPHeaderField: "refresh-token")
+    }
+    
+    func didRequest(_ urlRequest: URLRequest, with response: HTTPURLResponse, failDueToAuthenticationError error: any Error) -> Bool {
+        return response.statusCode == 401
+    }
+    
+    func isRequest(_ urlRequest: URLRequest, authenticatedWith credential: GraceLogAuthenticationCredential) -> Bool {
+        let bearerToken = HTTPHeader.authorization(bearerToken: credential.accessToken).value
+        return urlRequest.headers["Authorization"] == bearerToken
+    }
+    
+    func refresh(_ credential: GraceLogAuthenticationCredential, for session: Alamofire.Session, completion: @escaping (Result<GraceLogAuthenticationCredential, any Error>) -> Void) {
+        
+        let url = "\(baseURL)/auth/refresh"
+        
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(credential.refreshToken)"
+        ]
+        
+        AF.request(url, method: .post, headers: headers)
+            .validate()
+            .responseDecodable(of: GraceLogResponseDTO<SignInResponseDTO>.self) { response in
+                switch response.result {
+                case .success(let value):
+                    if value.code == 200 {
+                        let newCredential = GraceLogAuthenticationCredential(
+                            accessToken: value.data.accessToken,
+                            refreshToken: value.data.refreshToken,
+                            expiredAt: Date(timeIntervalSinceNow: 60 * 120)
+                        )
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
--- GraceLog/Source/Data/Network/Service/Auth/AuthService.swift
+++ GraceLog/Source/Data/Network/Service/Auth/AuthService.swift
@@ -15,12 +15,15 @@ struct AuthService {
             AF.request(AuthTarget.signIn(request))
                 .validate()
                 .responseDecodable(of: GraceLogResponseDTO<SignInResponseDTO>.self) { response in
+                    print("로그인 \(response)")
                     switch response.result {
                     case .success(let response):
                         if response.code == 200 {
+                            print("로그인 성공 \(response.data)")
                             single(.success(response.data))
                         }
                     case .failure(let error):
+                        print("로그인 실패 \(error)")
                         single(.failure(error))
                     }
                 }
