6ca42fa61f585a7ca8331b96dff064795445dadf
SangJLee1103 <sangjlee1103@gmail.com>
2025-05-03 16:54:11 +0900

    feat: 키체인 기능 구현 (토큰 저장)

---
+++ GraceLog/Source/KeyChain/KeychainService.swift
@@ -0,0 +1,51 @@
+//
+//  KeychainService.swift
+//  GraceLog
+//
+//  Created by 이상준 on 5/2/25.
+//
+
+import Foundation
+
+protocol KeychainService {
+    var accessToken: String? { get set }
+    var refreshToken: String? { get set }
+}
+
+class KeychainServiceImpl: KeychainService {
+    static let shared = KeychainServiceImpl()
+    private init() {}
+    
+    struct Key {
+        static let accessToken = "accessToken"
+        static let refreshToken = "refreshToken"
+    }
+    
+    private let keychainAccess = KeyChainAccessImpl()
+    
+    var accessToken: String? {
+        get { keychainAccess.get(Key.accessToken) }
+        set {
+            if accessToken != nil {
+                keychainAccess.save(Key.accessToken, newValue ?? "")
+            } else {
+                keychainAccess.remove(Key.accessToken)
+            }
+        }
+    }
+    
+    var refreshToken: String? {
+        get { keychainAccess.get(Key.refreshToken) }
+        set {
+            if refreshToken != nil {
+                keychainAccess.save(Key.refreshToken, newValue ?? "")
+            } else {
+                keychainAccess.remove(Key.refreshToken)
+            }
+        }
+    }
+    
+    func isLoggedIn() -> Bool {
+        return accessToken != nil
+    }
+}
