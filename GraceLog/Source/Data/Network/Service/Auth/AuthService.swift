16a87dd49fa2b77aa202477b4802698fb5a65ae4
이상준 <devjun@matazoo.net>
2025-06-05 14:39:29 +0900

    refactor: AuthService 리팩토링

--- GraceLog/Source/Data/Network/Service/Auth/AuthService.swift
+++ GraceLog/Source/Data/Network/Service/Auth/AuthService.swift
@@ -13,15 +13,21 @@ struct AuthService {
     func signIn(request: SignInRequestDTO) -> Single<SignInResponseDTO> {
         return .create { single in
             AF.request(AuthTarget.signIn(request))
-                .validate(statusCode: 200..<300)
                 .responseDecodable(of: GraceLogResponseDTO<SignInResponseDTO>.self) { response in
                     switch response.result {
                     case .success(let response):
-                        if response.code == 200 {
-                            single(.success(response.data))
+                        if response.code == 200, let data = response.data {
+                            single(.success(data))
+                        }  else {
+                            let error = APIError.serverError(
+                                code: response.code,
+                                message: response.message
+                            )
+                            single(.failure(error))
                         }
                     case .failure(let error):
-                        single(.failure(error))
+                        let apiError = APIError.networkError(error)
+                        single(.failure(apiError))
                     }
                 }
             return Disposables.create()
